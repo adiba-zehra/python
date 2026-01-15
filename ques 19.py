4d6c37c1483489a8c31759bbb76c7b7c9e2e2441
Adiba Zehra <157864970+adiba-zehra@users.noreply.github.com>
2026-01-16 00:34:04 +0530

    Delete ques 19.py

--- ques 19.py
+++
@@ -1,5 +0,0 @@
-import string
-
-user_input = input("Enter a string: ")
-no_punctuation = user_input.translate(str.maketrans('', '', string.punctuation))
-print("String without punctuation:", no_punctuation)

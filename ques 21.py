782e171dc7c455a64c6345e89f771f29849889ea
Adiba Zehra <157864970+adiba-zehra@users.noreply.github.com>
2026-01-19 23:30:51 +0530

    Delete ques 21.py

--- ques 21.py
+++
@@ -1,3 +0,0 @@
-elements = list(map(int, input("Enter elements separated by spaces: ").split()))
-specific_element = int(input("Enter the element to count: "))
-print(f"The element {specific_element} occurs {elements.count(specific_element)} times.")
